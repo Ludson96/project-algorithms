a2ad2ceaa4d9ea5538d641491146d752702d3431
Ludson Pereira <ludson_ps27@hotmail.com>
2023-04-25 20:55:11 -0300

    feat: :sparkles: add função palindrome_recurse
    função verifica de forma recursiva se a palavra é um palindromo e retorna valor boolean

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(
+    word: str, low_index: int, high_index: int
+) -> bool:
+    if not word:
+        return False
+
+    if high_index <= low_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,7 +5,7 @@ from challenges.challenge_encrypt_message import encrypt_message
 def test_encrypt_message():
     assert encrypt_message("mensagem", 3) == "nem_megas"
     assert encrypt_message("mensagem", 4) == "mega_snem"
-    assert encrypt_message("mensagem", 10) == "megasnem"
+    assert encrypt_message("mensagem", 9) == "megasnem"
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(123456, 3)
     with pytest.raises(TypeError, match="tipo inválido para key"):
