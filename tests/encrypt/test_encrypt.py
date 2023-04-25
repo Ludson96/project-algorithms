14fbb5ae2b49e5fa4556f1ea8dc26ecc09d970d5
Ludson Pereira <ludson_ps27@hotmail.com>
2023-04-25 18:19:11 -0300

    test: :white_check_mark: add test para função excrypt
    função escripta uma mensagem passada como parametro

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("mensagem", 3) == "nem_megas"
+    assert encrypt_message("mensagem", 4) == "mega_snem"
+    assert encrypt_message("mensagem", 10) == "megasnem"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 3)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("mensagem", "3")
