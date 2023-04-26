36a7e5b773a87f815c1fc6ead639f8c139729cff
Ludson Pereira <ludson_ps27@hotmail.com>
2023-04-26 11:58:01 -0300

    feat: :sparkles: add função find_the_duplicate
    função procura numeros duplicados em uma lista passada por parametro

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,32 @@
+def binary_search(numbers, target):
+    start = 0
+    end = len(numbers) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if numbers[mid] == target:
+            return mid
+
+        if target < numbers[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return False
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    if len(nums) < 2 or not nums:
+        return False
+
+    for i in range(len(nums)):
+        if not isinstance(nums[i], int) or nums[i] < 1:
+            return False
+        else:
+            index = binary_search(nums[i + 1:], nums[i])
+            if index is not False:
+                return nums[index + i + 1]
+
+    return False
