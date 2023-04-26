312ddfd622ef81ea643f0ec1bf0aaee8a4f23720
Ludson Pereira <ludson_ps27@hotmail.com>
2023-04-26 10:21:46 -0300

    feat: :sparkles: add função anagrams
    função utiliza o metodo de ordenação quicksort para ordernar a palavra

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,32 @@
+def quicksort(palavra):
+    if len(palavra) <= 1:
+        return palavra
+    else:
+        pivot = palavra[-1]
+        menores = []
+        maiores = []
+        iguais = []
+        for letra in palavra:
+            if letra < pivot:
+                menores.append(letra)
+            elif letra > pivot:
+                maiores.append(letra)
+            else:
+                iguais.append(letra)
+        return (
+            quicksort("".join(menores))
+            + "".join(iguais)
+            + quicksort("".join(maiores))
+        )
+
+
 def is_anagram(first_string, second_string):
-    raise NotImplementedError
+    if not first_string and not second_string:
+        return (first_string, second_string, False)
+    primeira, segunda = quicksort(first_string.lower()), quicksort(
+        second_string.lower()
+    )
+    if primeira == segunda:
+        return (primeira, segunda, True)
+    else:
+        return (primeira, segunda, False)
